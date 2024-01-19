7c0e3873c3b280d3540df7265d12b2123d8f6956
thorongil05 <officialantonio@outlook.com>
2024-01-19 19:33:08 +0100

    Remove bonds module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { BondsFormComponent } from './bonds-form/bonds-form.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    BondsFormComponent,
-    SideBarComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, SideBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
