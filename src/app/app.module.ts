fab39e238cdc9801af3d8aa202417ab38cc61230
thorongil05 <officialantonio@outlook.com>
2024-01-19 19:19:17 +0100

    Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BondsFormComponent } from './bonds-form/bonds-form.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BondsFormComponent,
+    SideBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/bonds-form/bonds-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonds-form',
+  templateUrl: './bonds-form.component.html',
+  styleUrls: ['./bonds-form.component.scss']
+})
+export class BondsFormComponent {
+
+}
