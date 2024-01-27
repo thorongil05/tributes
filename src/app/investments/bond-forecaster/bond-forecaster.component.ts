9a9a69f1c2d80b871ce7b94477ccfb379ca3b8f6
thorongil05 <officialantonio@outlook.com>
2024-01-27 13:38:36 +0100

    Add form in bond-forecaster component

--- src/app/investments/bond-forecaster/bond-forecaster.component.ts
+++ src/app/investments/bond-forecaster/bond-forecaster.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Bond } from 'src/app/model/bond';
+import { BondConstants } from 'src/app/model/bond-constants';
 import { BondType } from 'src/app/model/bond-type';
 import { InvestmentsService } from 'src/app/services/investments.service';
 
@@ -8,7 +11,18 @@ import { InvestmentsService } from 'src/app/services/investments.service';
   styleUrls: ['./bond-forecaster.component.scss'],
 })
 export class BondForecasterComponent implements OnInit {
+  private _bond?: Bond;
   private _bondTypes: BondType[] = [];
+  private _formGroup: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern(BondConstants.FINECO_NAME_PATTERN),
+    ]),
+    isin: new FormControl('', [
+      Validators.required,
+      Validators.pattern(BondConstants.ISIN_PATTERN),
+    ]),
+  });
 
   constructor(private investmentsService: InvestmentsService) {}
 
@@ -18,5 +32,18 @@ export class BondForecasterComponent implements OnInit {
 
   public get bondTypes(): BondType[] {
     return this._bondTypes;
+  }
+
+  public get formGroup(): FormGroup {
+    return this._formGroup;
+  }
+
+  public compute() {
+    console.log(this.formGroup);
+    if (!this.formGroup.valid) {
+      alert('La form non é valida');
+      return;
+    }
+    alert('La form é valida');
   }
 }
--- src/app/investments/investments.module.ts
+++ src/app/investments/investments.module.ts
@@ -5,9 +5,14 @@ import { InvestmentsRoutingModule } from './investments-routing.module';
 import { BondForecasterComponent } from './bond-forecaster/bond-forecaster.component';
 import { BondsOverviewComponent } from './bonds-overview/bonds-overview.component';
 import { SharesOverviewComponent } from './shares-overview/shares-overview.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [BondForecasterComponent, BondsOverviewComponent, SharesOverviewComponent],
-  imports: [CommonModule, InvestmentsRoutingModule],
+  declarations: [
+    BondForecasterComponent,
+    BondsOverviewComponent,
+    SharesOverviewComponent,
+  ],
+  imports: [CommonModule, InvestmentsRoutingModule, ReactiveFormsModule],
 })
 export class InvestmentsModule {}
