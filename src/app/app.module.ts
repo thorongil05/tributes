20e4d3fb9a72c45d4b9a2d1c58bffa6fb879e612
thorongil05 <officialantonio@outlook.com>
2024-12-25 11:09:47 +0100

    feat: add internationalization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,17 @@ import { InvestmentsModule } from './investments/investments.module';
 import { FeaturesModule } from './features/features.module';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import localeIt from '@angular/common/locales/it';
+import localeEn from '@angular/common/locales/en';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeIt);
+registerLocaleData(localeEn);
+
+const browserLang = navigator.language;
+
+console.log(browserLang);
 
 @NgModule({
   declarations: [AppComponent, SideBarComponent],
@@ -19,7 +30,10 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     FeaturesModule,
     NgxGraphModule,
   ],
-  providers: [provideAnimationsAsync()],
+  providers: [
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'en-US' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
